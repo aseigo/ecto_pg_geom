385024ffcd314b2385ceec646e820a09799e60e2
Aaron Seigo <aaron@nomoko.world>
2019-08-29 11:58:01 +0200

    EctoPgGeo.Point.point_equality -> EctoPgGeo.geometric_equality
    the equality operator is needed for more than only points

--- lib/ecto_pg_geom.ex
+++ lib/ecto_pg_geom.ex
@@ -3,4 +3,17 @@ defmodule EctoPgGeom do
   A collection of modules allowing [Postgres Geometry types](https://www.postgresql.org/docs/9.4/datatype-geometric.html)
   to be used in Ecto, via the Postgrex library.
   """
+
+  @doc """
+  A macro that generates an Ecto fragment which compares the equality of two
+  geometries. Required for testing equality of geometric types such as points
+  and polygons which do not have a functional `=` operator.
+
+  To use `import EctoPgGeom, only: [geometric_equality: 2]` and then you may
+  use in e.g. Ecto query where clauses: `geometric_equality(t.point, point)`
+  """
+  defmacro geometric_equality(p1, p2) do
+    quote do: fragment("? ~= ?", unquote(p1), ^unquote(p2))
+  end
+
 end
--- lib/ecto_pg_geom/point.ex
+++ lib/ecto_pg_geom/point.ex
@@ -2,10 +2,6 @@ defmodule EctoPgGeom.Point do
   @behaviour Ecto.Type
   def type, do: Postgrex.Point
 
-  defmacro point_equality(p1, p2) do
-    quote do: fragment("? ~= ?", unquote(p1), ^unquote(p2))
-  end
-
   # Casting from input into point struct
   def cast(value = %Postgrex.Point{}), do: {:ok, value}
   def cast({x, y}), do: {:ok, %Postgrex.Point{x: x, y: y}}
