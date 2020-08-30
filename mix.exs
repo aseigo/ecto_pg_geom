19e1d3d36ddf5116dae3a07a88bee9249f3de2cc
Aaron Seigo <aseigo@kde.org>
2020-08-30 12:58:18 +0200

    bump version for doc fixes

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule EctoPgGeom.MixProject do
   def project do
     [
       app: :ecto_pg_geom,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.7",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
